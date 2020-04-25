c572745f354c1d87f4c412581b3dfd67551b904f
Manish Poduval <manishpoduval@hotmail.com>
2020-04-25 14:20:41 +0530

    intro assignment 2

---
+++ intro-template/script.js
@@ -0,0 +1,37 @@
+function writeYourCodeHere() {
+    /*Write you jQuery code here*/
+  
+  /**
+   * The set of statements that are executed in the browser console to try out
+   * Use console.log() to check if you're getting the elements correctly
+   * jQuery selectors
+   */
+  
+  // Get all img elements on the page
+  
+  
+  // Get all elements with a class of `card_image`
+  
+  
+  // Get the footer by its ID
+  
+  
+  // All paragrapgh elements in the `footer`
+  
+  // Get all header elements (h1-h6)
+  
+  
+  // Get first element on the page
+  
+  
+  // Get the last `div` on the page
+  
+  
+  // Get the last `img` on the page
+  
+  
+  // Get all elements that have an attribute of `href`
+  
+  }
+  
+  $(writeYourCodeHere());
